4f11b13b878c5468c83a4539d91e80941c404140
SAAYALI <saayalikashyap@gmail.com>
2026-08-30 18:36:22 +0530

    Add MailPilot AI extension

---
+++ extension/src/popup.js
@@ -0,0 +1,7 @@
+document.getElementById("summarizeBtn").addEventListener("click", function () {
+    alert("Summarize Email clicked!");
+});
+
+document.getElementById("replyBtn").addEventListener("click", function () {
+    alert("Generate Reply clicked!");
+});
